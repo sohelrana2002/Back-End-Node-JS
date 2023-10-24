e845f7042ae1c3ba38ba3bdc90d7f3e86bfd0d60
SOHEL RANA <sohelrana104322@gmail.com>
2023-10-24 15:12:15 +0600

    add: File-System-Async Folder

---
+++ File-System-Async/index.js
@@ -0,0 +1,31 @@
+// ==============CRUD OPERATION USING Async(callback function)=========
+// ============target============
+// 1. create a folder
+// 2. create file
+// 3. write file
+// 4. update content
+// 5. rename file
+// 6. delete file
+
+const fs = require("fs");
+
+
+// ==========create folder-==============
+// fs.mkdir("CRUD-Async", (err) => console.log("Create folder successfully"));
+
+
+// ==========create file===========
+// fs.writeFile("CRUD-Async/data.txt", "New data.txt is created", (err) => {
+//     console.log("Create file successfully")
+// })
+
+
+// =======update file content=============
+// fs.appendFile("CRUD-Async/data.txt", ", update content without overriding.", (err) => {
+//     console.log("update content successfully");
+// });
+
+// ===========rename file==========
+fs.rename("CRUD-Async/data.txt", "CRUD-Async/readWrite.txt", (err) => {
+    console.log("Rename file name successfully");
+});
