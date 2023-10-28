463a2718bc88f9b2bdaafc93d28b8b4d68cf08d1
SOHEL RANA <sohelrana104322@gmail.com>
2023-10-28 23:14:47 +0600

    add:send json and object

---
+++ Express-JS/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+
+// console.log(app);
+// ---------express js have lot of method----
+// ----method work with like that---
+// get(route, callback);
+/*mosly work with operation like that(CRUD)
+1. API 
+2. get- read
+3. post- create new data
+4. put - update data
+5. delete - delete your data
+*/
+
+// ----------create routing-----------
+app.get("/", (req, res) =>{
+    res.send("Hello from the express home page")
+});
+
+app.get("/about", (req, res) =>{
+    res.send("This is about page from express")
+});
+
+
+// ==========work with json data==============
+app.get("/tempature", (req, res) =>{
+    res.send({
+        currentTemp: "26C",
+        previousDay: "24C"
+    });
+});
+
+// ==========work with array with object==============
+app.get("/student", (req, res) =>{
+    res.send([
+        {
+            id: 1,
+            name: "Sohel Rana",
+            dept: "CSE",
+            sec: "B"
+        },
+        {
+            id: 2,
+            name: "Fahim Rahman",
+            dept: "EEE",
+            sec: "A"
+        },
+    ]);
+});
+
+
+// ==========work with array with object(json method)==============
+app.get("c", (req, res) =>{
+    res.json([
+        {
+            id: 1,
+            name: "Sohel Rana",
+            dept: "CSE",
+            sec: "B"
+        },
+        {
+            id: 2,
+            name: "Fahim Rahman",
+            dept: "EEE",
+            sec: "A"
+        },
+    ]);
+});
+
+/*Uing res.json() method have some benifits like non-object, null, undefined and not valid json to convert the valid json*/
+
+
+
+
+app.listen(2000, "192.168.0.102", () =>{
+    console.log("Listing from port 2000");
+});
+
+
