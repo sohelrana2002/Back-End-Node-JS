3bf89b7dbc983c05ce69973466c48e57db6f3a14
SOHEL RANA <sohelrana104322@gmail.com>
2023-10-31 00:11:09 +0600

    add: more operation

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -28,6 +28,13 @@
 // output
 // switched on db employee
 
+// =========how to check active database===========
+// command 
+// db 
+
+// output 
+// your active database
+
 // ==========how to insert one data in collection=========
 // command 
 //db.collectionData.insertOne({});
@@ -94,8 +101,168 @@
 //         age: 30,
 //         sub: 'Cse'
 //     },
+//     {
+//       _id: ObjectId("653feea2efc708dd052a8065"),
+//       name: 'Feroz Kobir',
+//       age: 27,
+//       sub: 'BBA',
+//       active: true
+//     }
 
 // ]
+
+
+// ==========read specific value from collections=========
+// command 
+// db.collectionDataName.find({query: "valueName"});
+
+// example 
+// db.employeeData.find({name: "Sohel Rana"}).pretty()
+
+// output 
+// [
+//     {
+//       _id: ObjectId("653fdb9358cc65f42f086b4c"),
+//       name: 'Sohel Rana',
+//       age: 21,
+//       sub: 'CSE'
+//     }
+// ]
+
+
+
+// ==========read specific value from collections (only name)=========
+// command 
+// db.collectionDataName.find({query: "valueName"}, projection);
+
+// example 
+// db.employeeData.find({name: "Sohel Rana"}, {name: 1}).pretty()
+
+// output 
+// [
+//     {
+//       _id: ObjectId("653fdb9358cc65f42f086b4c"),
+//       name: 'Sohel Rana',
+//     }
+// ]
+
+
+
+
+// ==========read specific value from collections (only name)
+//  without id also=========
+// command 
+// db.collectionDataName.find({query: "valueName"}, projection);
+
+// example 
+// db.employeeData.find({name: "Sohel Rana"}, {_id: 0, name: 1}).pretty()
+
+// output 
+// [
+//     {
+//       name: 'Sohel Rana',
+//     }
+// ]
+
+
+
+// ==========read without specific value from collections =========
+// command 
+// db.collectionDataName.find({query: "valueName"}, projection);
+
+// example 
+// db.employeeData.find({name: "Sohel Rana"}, {name: 0}).pretty()
+
+// output 
+// [{
+//     _id: ObjectId("653fdb9358cc65f42f086b4c"),
+//     age: 21, sub: 'CSE'
+// }]
+
+
+
+// ===========how to filtering in collention========
+// command 
+// db.employeeDataName.find({query: "valueName"}).pretty()
+
+// example 
+// db.employeeData.find({active: ture}).pretty()
+
+// output 
+// [
+//     {
+//       _id: ObjectId("653feea2efc708dd052a8065"),
+//       name: 'Feroz Kobir',
+//       age: 27,
+//       sub: 'BBA',
+//       active: true
+//     },
+//     {
+//       _id: ObjectId("653fef5defc708dd052a8066"),
+//       name: 'Nazmus Sakib',
+//       age: 23,
+//       sub: 'Animal Husbandy',
+//       active: true
+//     }
+//   ]
+
+
+// =========filter your data from collection with only one data==========
+// command 
+// db.employeeDataName.find({query: "valueName"}).pretty().limit()
+
+// example 
+// db.employeeData.find({active: ture}).pretty().limit(1)
+
+// output 
+// [
+//     {
+//       _id: ObjectId("653feea2efc708dd052a8065"),
+//       name: 'Feroz Kobir',
+//       age: 27,
+//       sub: 'BBA',
+//       active: true
+//     }
+// ]
+
+
+
+
+// =========filter your data from collection with only one data (another method)==========
+// command 
+// db.employeeDataName.find({query: "valueName"})
+
+// example 
+// db.employeeData.findOne({active: ture})
+
+// output 
+// {
+//     _id: ObjectId("653feea2efc708dd052a8065"),
+//     name: 'Feroz Kobir',
+//     age: 27,
+//     sub: 'BBA',
+//     active: true
+// }
+
+
+
+// =========filter your data from collection with only one data skip first field==========
+// command 
+// db.employeeDataName.find({query: "valueName"}).pretty().limit(1).skip(1)
+
+// example 
+// db.employeeData.find({active: ture}).pretty().limit(1).skip(1)
+
+// output 
+// [
+//     {
+//       _id: ObjectId("653fef5defc708dd052a8066"),
+//       name: 'Nazmus Sakib',
+//       age: 23,
+//       sub: 'Animal Husbandy',
+//       active: true
+//     }
+//   ]
 
 
 
