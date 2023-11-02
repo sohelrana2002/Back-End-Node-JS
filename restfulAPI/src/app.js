a90e6ab378ceb3b41af59df619135a31ee6c51be
SOHEL RANA <sohelrana104322@gmail.com>
2023-11-02 23:15:22 +0600

    add:POST Method

--- restfulAPI/src/app.js
+++ restfulAPI/src/app.js
@@ -5,12 +5,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+app.use(express.json());
+
+
 // app.get("/", (req, res) =>{
 //     res.send("from home page ")
 // });
 
-app.post("/student-registration", (req, res) =>{
-    res.send("Student Registration page");
+app.post("/student-registration", async(req, res) =>{
+    console.log(req.body);
+    try{
+        const user = new Student(req.body);
+        const userData = await user.save();
+        console.log(userData);
+        res.status(201).send(userData);
+    }catch(err){
+        res.status(404).send(err);
+    }
+
+    // res.send("Student Registration page");
 });
 
 app.listen(port,"192.168.43.189", () =>{
