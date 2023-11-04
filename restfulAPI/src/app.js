35f0e7390d28e5ca747d9bab4e89b2da1dc60f72
SOHEL RANA <sohelrana104322@gmail.com>
2023-11-04 14:41:43 +0600

    add:update or patch

--- restfulAPI/src/app.js
+++ restfulAPI/src/app.js
@@ -53,25 +53,44 @@ app.get("/student-registration/:id", async (req, res) =>{
         // =======getUser singular for only id======
         const getUser = await Student.findById(_id);
         if(!getUser){
-            res.status(201).send();
+            res.status(404).send();
         }else{
-            res.status(404).send(getUser);
+            res.status(201).send(getUser);
             // console.log(getUser);
         }
     }catch(err){
-        res.send(err);
+         res.status(404).send(err);
     }
 });
 
-// ========read/get user data(individual with name)=======
-app.get("/student-registration/:name", async (req, res) =>{
+// // ========read/get user data(individual with name)=======
+// app.get("/student-registration/:name", async (req, res) =>{
+//     try{
+//         const name = req.params.name;
+//         const getName = await Student.find(name);
+//         res.send(getName);
+//         console.log(getName);
+//     }catch(err){
+//         res.send(err);
+//     } 
+// });
+
+
+// =======update user data though id=======
+app.patch("/student-registration/:id", async (req, res) =>{
     try{
-        const name = req.params.name;
-        const getName = await Student.find(name);
-        res.send(getName);
-        console.log(getName);
+        const _id = req.params.id;
+        const updateUser = await Student.findByIdAndUpdate(_id, req.body, {
+            new: true
+        });
+
+        if(!updateUser){
+            res.status(404).send();
+        }else{
+            res.status(201).send(updateUser);
+        }
     }catch(err){
-        res.send(err);
+        res.status(404).send(err);
     }
 })
 
